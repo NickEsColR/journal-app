b9b29019a9a3e21d0d28a346a0fad27a7cc7df0d
NickEsColR <nicoesol0818@gmail.com>
2024-05-01 17:57:37 -0500

    style: add sideBar

--- src/journal/components/Navbar.jsx
+++ src/journal/components/Navbar.jsx
@@ -12,263 +12,27 @@ export const Navbar = ({ drawerWidth }) => {
         >
             <Toolbar>
                 <IconButton
-                color="inherit"
-                edge="start"
-                sx={{
-                    mr: 2,
-                    display: { sm: "none" },
-                }}
+                    color="inherit"
+                    edge="start"
+                    sx={{
+                        mr: 2,
+                        display: { sm: "none" },
+                    }}
                 >
                     <MenuOutlined />
                 </IconButton>
 
-                <Grid container direction={"row"} justifyContent={"space-between"} alignItems={'center'}>
-                    <Typography variant="h6" noWrap component={'div'}>JournalApp</Typography>
+                <Grid
+                    container
+                    direction={"row"}
+                    justifyContent={"space-between"}
+                    alignItems={"center"}
+                >
+                    <Typography variant="h6" noWrap component={"div"}>
+                        JournalApp
+                    </Typography>
                     <IconButton color="error">
                         <LogoutOutlined />
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                        ---------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-                        
                     </IconButton>
                 </Grid>
             </Toolbar>
--- src/journal/layout/JournalLayout.jsx
+++ src/journal/layout/JournalLayout.jsx
@@ -1,5 +1,5 @@
-import { Box } from "@mui/material";
-import { Navbar } from "../components";
+import { Box, Toolbar } from "@mui/material";
+import { Navbar, SideBar } from "../components";
 
 const drawerWidth = 240;
 
@@ -9,9 +9,11 @@ export const JournalLayout = ({ children }) => {
             {/* Navbar */}
             <Navbar drawerWidth={drawerWidth}/>
             {/* Sidebar */}
+            <SideBar drawerWidth={drawerWidth}/>
 
             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
                 {/* Toolbar */}
+                <Toolbar />
                 {children}
             </Box>
         </Box>
