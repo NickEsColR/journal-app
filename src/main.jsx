f48f23391daed2dc65c6a4b11d6a8854f69d506a
NickEsColR <nicoesol0818@gmail.com>
2024-05-02 08:10:10 -0500

    feat: add redux to auth

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles.css'
-import { JournalApp } from './JournalApp'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles.css";
+import { JournalApp } from "./JournalApp";
+import { Provider } from "react-redux";
+import { store } from "./store";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <JournalApp />
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <Provider store={store}>
+            <JournalApp />
+        </Provider>
+    </React.StrictMode>
+);
