b98f44176756d7877804a9e49c70992e18683959
NickEsColR <nicoesol0818@gmail.com>
2024-05-02 11:49:08 -0500

    feat: add register page validations

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,12 +1,46 @@
-import { Google } from "@mui/icons-material";
-import { Button, Grid, Typography, TextField, Link } from "@mui/material";
+import { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
+import { Button, Grid, Typography, TextField, Link } from "@mui/material";
 import { AuthLayout } from "../layout";
+import { useForm } from "../../hooks";
+
+const formInitialState = {
+    displayName: "",
+    email: "",
+    password: "",
+};
+
+const formValidations = {
+    email: [(value) => value.includes("@"), "Debe ser un correo válido"],
+    password: [
+        (value) => value.length >= 6,
+        "La contraseña debe tener al menos 6 caracteres",
+    ],
+    displayName: [(value) => value.trim().length > 0, "El nombre es requerido"],
+};
 
 export const RegisterPage = () => {
+    const [formSubmitted, setFormSubmitted] = useState(false)
+
+    const {
+        displayName,
+        email,
+        password,
+        onInputChange,
+        displayNameInvalidMessage,
+        emailInvalidMessage,
+        passwordInvalidMessage,
+    } = useForm(formInitialState, formValidations);
+
+    const onSubmit = (e) => {
+        e.preventDefault();
+        setFormSubmitted(true);
+
+    };
+
     return (
         <AuthLayout title="Crear cuenta">
-            <form>
+            <form onSubmit={onSubmit}>
                 <Grid container>
                     <Grid item xs={12} sx={{ mt: 2 }}>
                         <TextField
@@ -14,6 +48,11 @@ export const RegisterPage = () => {
                             type="text"
                             placeholder="Nombre completo"
                             fullWidth
+                            name="displayName"
+                            value={displayName}
+                            onChange={onInputChange}
+                            error={!!displayNameInvalidMessage && formSubmitted}
+                            helperText={displayNameInvalidMessage}
                         />
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
@@ -22,6 +61,11 @@ export const RegisterPage = () => {
                             type="email"
                             placeholder="correo@google.com"
                             fullWidth
+                            name="email"
+                            value={email}
+                            onChange={onInputChange}
+                            error={!!emailInvalidMessage && formSubmitted}
+                            helperText={emailInvalidMessage}
                         />
                     </Grid>
                     <Grid item xs={12} sx={{ mt: 2 }}>
@@ -30,19 +74,26 @@ export const RegisterPage = () => {
                             type="password"
                             placeholder="contraseña"
                             fullWidth
+                            name="password"
+                            value={password}
+                            onChange={onInputChange}
+                            error={!!passwordInvalidMessage && formSubmitted}
+                            helperText={passwordInvalidMessage}
                         />
                     </Grid>
 
                     <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
                         <Grid item xs={12}>
-                            <Button variant="contained" fullWidth>
+                            <Button variant="contained" fullWidth type="submit">
                                 Crear cuenta
                             </Button>
                         </Grid>
                     </Grid>
 
                     <Grid container direction={"row"} justifyContent="end">
-                    <Typography sx={{ mr: 1 }}>¿Ya tienes una cuenta?</Typography>
+                        <Typography sx={{ mr: 1 }}>
+                            ¿Ya tienes una cuenta?
+                        </Typography>
                         <Link
                             component={RouterLink}
                             color={"inherit"}
--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,7 +1,12 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export const useForm = (initialForm = {}) => {
+export const useForm = (initialForm = {}, formValidations = {}) => {
     const [formState, setFormState] = useState(initialForm);
+    const [formInvalidMsg, setFormInvalidMsg] = useState({});
+
+    useEffect(() => {
+        validateForm();
+    }, [formState]);
 
     const onInputChange = ({ target }) => {
         const { name, value } = target;
@@ -15,10 +20,28 @@ export const useForm = (initialForm = {}) => {
         setFormState(initialForm);
     };
 
+    const validateForm = () => {
+        const formInvalidMsgValues = {};
+
+        for (const formField of Object.keys(formValidations)) {
+            const [fn, errorMessage] = formValidations[formField];
+
+            formInvalidMsgValues[`${formField}InvalidMessage`] = fn(
+                formState[formField]
+            )
+                ? null
+                : errorMessage;
+
+            setFormInvalidMsg(formInvalidMsgValues);
+        }
+    };
+
     return {
         ...formState,
         formState,
         onInputChange,
         onResetForm,
+
+        ...formInvalidMsg
     };
 };
