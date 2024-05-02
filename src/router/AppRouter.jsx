9b61d5e0e7db01f3293ee0c61e363faebc500bdb
NickEsColR <nicoesol0818@gmail.com>
2024-05-02 18:04:18 -0500

    style: add loader when is authenting

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,8 +1,16 @@
+import { useSelector } from "react-redux";
 import { Route, Routes } from "react-router-dom";
 import { AuthRoutes } from "../auth";
 import { JournalPage } from "../journal";
+import { CheckingAuth } from "../ui";
 
 export const AppRouter = () => {
+
+    const { status} = useSelector((state) => state.auth);
+
+    if(status === 'checking') {
+        return <CheckingAuth />
+    };
     return (
         <Routes>
             <Route path="/auth/*" element={<AuthRoutes />} />
