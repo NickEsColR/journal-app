ff7f8b080e85b64e9d38baa64406e1f306a5a602
NickEsColR <nicoesol0818@gmail.com>
2024-05-03 10:19:05 -0500

    style: add notes to the sidebar

--- src/journal/components/SideBar.jsx
+++ src/journal/components/SideBar.jsx
@@ -1,23 +1,19 @@
-import { TurnedInNot } from "@mui/icons-material";
+import { useSelector } from "react-redux";
 import {
     Box,
     Divider,
     Drawer,
-    Grid,
     List,
-    ListItem,
-    ListItemButton,
-    ListItemIcon,
-    ListItemText,
     Toolbar,
     Typography,
 } from "@mui/material";
-import React from "react";
-import { useSelector } from "react-redux";
+
+import { SideBarItem } from "./";
 
 export const SideBar = ({ drawerWidth }) => {
 
     const {displayName} = useSelector(state => state.auth);
+    const {notes} = useSelector(state => state.journal);
     return (
         <Box
             component="nav"
@@ -45,31 +41,8 @@ export const SideBar = ({ drawerWidth }) => {
                 <Divider />
 
                 <List>
-                    {[
-                        "Enero",
-                        "Febrero",
-                        "Marzo",
-                        "Abril",
-                        "Mayo",
-                        "Junio",
-                        "Julio",
-                        "Agosto",
-                        "Septiembre",
-                        "Octubre",
-                        "Noviembre",
-                        "Diciembre",
-                    ].map((text) => (
-                        <ListItem key={text} disablePadding>
-                            <ListItemButton>
-                                <ListItemIcon>
-                                    <TurnedInNot />
-                                </ListItemIcon>
-                                <Grid container>
-                                    <ListItemText primary={text} />
-                                    <ListItemText secondary="28" />
-                                </Grid>
-                            </ListItemButton>
-                        </ListItem>
+                    {notes.map((note) => (
+                        <SideBarItem key={note.id} {...note} />
                     ))}
                 </List>
             </Drawer>
