2b93c66c0df430b9c9d33a2a01915e52f71b1f4e
NickEsColR <nicoesol0818@gmail.com>
2024-05-03 10:23:46 -0500

    feat: add select note and set it active

--- src/journal/components/SideBarItem.jsx
+++ src/journal/components/SideBarItem.jsx
@@ -1,15 +1,24 @@
+import { useMemo } from "react";
+import { useDispatch } from "react-redux";
 import { Grid, ListItem, ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import { TurnedInNot } from "@mui/icons-material";
-import { useMemo } from "react";
+
+import { setActiveNote } from "../../store/journal/JournalSlice";
 
 export const SideBarItem = ({title, body, id}) => {
+
+    const dispatch = useDispatch();
 
     const newTitle = useMemo(()=>{
         return title.length > 17 ? title.substring(0, 17) + '...': title;
     }, [title])
+
+    const onClickItem = () => {
+        dispatch(setActiveNote({id, title, body}));
+    }
     return (
         <ListItem disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={onClickItem}>
                 <ListItemIcon>
                     <TurnedInNot />
                 </ListItemIcon>
