24c45ad10e5ae184e61ff498ba1f8f3ca20985b9
NickEsColR <nicoesol0818@gmail.com>
2024-05-04 19:50:59 -0500

    test image upload

--- test/helpers/fileUpload.test.js
+++ test/helpers/fileUpload.test.js
@@ -1,17 +1,32 @@
 import { v2 as cloudinary } from "cloudinary";
+
+import { getEnvs } from "../../src/helpers";
 import { fileUpload } from "../../src/helpers/fileUpload";
 
+const {
+    VITE_CLOUD_NAME_CLOUDINARY,
+    VITE_API_KEY_CLOUDINARY,
+    VITE_API_SECRET_CLOUDINARY,
+    VITE_SECURE_CLOUDINARY,
+} = getEnvs();
+
 cloudinary.config({
-    cloud_name: "dmuhagilf",
-    api_key: "myApiKey",
-    api_secret: "mySecretKey",
-    secure: true,
+    cloud_name: VITE_CLOUD_NAME_CLOUDINARY,
+    api_key: VITE_API_KEY_CLOUDINARY,
+    api_secret: VITE_API_SECRET_CLOUDINARY,
+    secure: VITE_SECURE_CLOUDINARY,
 });
+
+const deleteImage = async (imageId) => {
+    await cloudinary.api.delete_resources(["journal-app/" + imageId], {
+        resource_type: "image",
+    });
+};
 
 describe("Pruebas en fileUpload", () => {
     test("debe de subir el archivo correctamente", async () => {
-        const imageUrl = "";
-        const resp = await fetch(imageUrl);
+        
+        const resp = await fetch("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwg0ljoWaCLWz88fHhWVkqwckULN7LPepSSg&s")
         const blob = await resp.blob();
         const file = new File([blob], "foto.jpg");
 
@@ -21,9 +36,8 @@ describe("Pruebas en fileUpload", () => {
         // Borrar imagen por ID
         const segments = url.split("/");
         const imageId = segments[segments.length - 1].replace(".jpg", "");
-        await cloudinary.api.delete_resources(['journal/'+imageId],{
-            resource_type: 'image'
-        });
+        console.log(imageId)
+        await deleteImage(imageId);
     });
 
     test("debe de retornar null", async () => {
